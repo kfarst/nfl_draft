10f850e3bb8a67e2c9d4bf066e9d5cc39032756d
Pairing Machine <pairing@edgecase.com>
2010-09-25 00:27:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NflDraft::Application.config.secret_token = '05ec40976612da1308174ce917d095de86369a1e68d71726660601c1d4cdcff2836e12ed5eaabaac6745f95fdb2c838575c3adc6c36f3c6d6048f5141c48dd7a'
