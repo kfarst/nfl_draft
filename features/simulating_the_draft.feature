e7d2deb4ccce58d83cfa5ddd868694b1fe0a5a55
Pairing Machine <pairing@edgecase.com>
2010-09-25 11:22:33 -0400

    WIP add cucumber features

---
+++ features/simulating_the_draft.feature
@@ -0,0 +1,14 @@
+Feature: Simulating the Draft
+
+  Background:
+    Given I am a User
+
+  Scenario: In order to get a real time update
+    Then I want to see the results of the draft by round and by NFL Team
+
+  Scenario: In order to see which players are still draftable
+    Then I want to be able to view all remaining players alphabetically by position
+
+  Scenario: In order to see only what is actively happening
+    Then I want to see the latest 3 picks and which team is next to pick
+
---
+++ features/step_definitions/simulating_the_draft_steps.rb
@@ -0,0 +1,16 @@
+Given /^I am a User$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I want to see the results of the draft by round and by NFL Team$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I want to be able to view all remaining players alphabetically by position$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I want to see the latest (\d+) picks and which team is next to pick$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
