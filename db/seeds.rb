f3801b6f4801ded5f520fd53d09cabf6ec40f681
Pairing Machine <pairing@edgecase.com>
2010-09-25 11:24:06 -0400

    WIP add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+require 'fastercsv'
+require 'ruby-debug'
+
+directory = File.dirname(__FILE__)
+
+players = "/seed_data/players.csv"
+puts "Loading player data..."
+
+ActiveRecord::Base.transaction do
+  FasterCSV.foreach(directory + players, :col_sep => ",", :headers => :first_row) do |row|
+    name, position = row
+    Player.create(:name => row[0], :position => row[1])
+  end
+end
+puts "Done"
+
+teams = "/seed_data/teams.csv"
+puts "Loading team data..."
+
+ActiveRecord::Base.transaction do
+  FasterCSV.foreach(directory + teams, :col_sep => ",", :headers => :first_row) do |row|
+    name, division = row
+    Team.create(:name => row[0], :position => row[1])
+  end
+end
+puts "Done"
+
+order = "/seed_data/order.csv"
+puts "Loading draft order data..."
+ActiveRecord::Base.transaction do
+  FasterCSV.foreach(directory + order, :col_sep => ",", :headers => :first_row) do |row|
+    round, pick, team = row
+    Order.create(:round => row[0], :pick => row[1], :team => row[2])
+  end
+end
+puts "Done"
