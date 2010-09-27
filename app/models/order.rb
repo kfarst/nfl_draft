850c8a31d882ab2d18347e260a8fb3d1e9199248
Pairing Machine <pairing@edgecase.com>
2010-09-27 09:01:49 -0400

    add logic for getting the next draft pick

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ActiveRecord::Base
   validates_presence_of :round, :pick, :team
   scope :draft, order("orders.round, orders.pick")
+  scope :unchosen, where("orders.chosen = ?", false)
+
+  def self.next
+    self.unchosen.first
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -24,9 +24,9 @@ describe Order do
 
   describe "#draft" do
     before(:each) do
-      pick1 = Factory(:order, :round => 1, :pick => 1)
-      pick2 = Factory(:order, :round => 1, :pick => 2)
-      pick3 = Factory(:order, :round => 2, :pick => 3)
+      @pick1 = Factory(:order, :round => 1, :pick => 1)
+      @pick2 = Factory(:order, :round => 1, :pick => 2)
+      @pick3 = Factory(:order, :round => 2, :pick => 3)
     end
 
     it "should return an array of Order objects" do
@@ -37,4 +37,48 @@ describe Order do
 
     it "should return the order of the draft"
   end
+
+  describe "#unchosen" do
+    before(:each) do
+      @pick1 = Factory(:order, :round => 1, :pick => 1, :chosen => true)
+      @pick2 = Factory(:order, :round => 1, :pick => 2)
+      @pick3 = Factory(:order, :round => 2, :pick => 3)
+    end
+
+    it "should return an array of Order objects" do
+      Order.draft.each do |pick|
+        pick.should be_a(Order)
+      end
+    end
+
+    it "should only return draft picks that have yet to be chosen" do
+      Order.unchosen.include?(@pick2).should be_true
+      Order.unchosen.include?(@pick3).should be_true
+    end
+
+    it "should not return draft picks that have already been chosen" do
+      Order.unchosen.include?(@pick1).should be_false
+    end
+  end
+
+  describe "#next" do
+    before(:each) do
+      @pick1 = Factory(:order, :round => 1, :pick => 1, :chosen => true)
+      @pick2 = Factory(:order, :round => 1, :pick => 2)
+      @pick3 = Factory(:order, :round => 2, :pick => 3)
+    end
+
+    it "should return an Order object" do
+      Order.next.should be_a(Order)
+    end
+
+    it "should return the next draft pick" do
+      Order.next.should == @pick2
+    end
+
+    it "should not return draft picks that have already been chosen" do
+      Order.next.should_not == @pick1
+    end
+  end
+
 end
